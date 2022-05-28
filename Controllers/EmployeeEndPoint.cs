068e80d16e8bbacbc59d7892fda4bf83af5f2e77
OLAWALE MARTINS <rytesoft@hotmail.com>
2022-05-28 22:33:36 +0100

    employee

---
+++ Controllers/EmployeeEndPoint.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAppConnect.Data;
+using WebAppConnect.Models;
+
+namespace WebAppConnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeEndPoint : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeEndPoint(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/EmployeeEndPoint
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployee()
+        {
+          if (_context.Employee == null)
+          {
+              return NotFound();
+          }
+            return await _context.Employee.ToListAsync();
+        }
+
+        // GET: api/EmployeeEndPoint/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+          if (_context.Employee == null)
+          {
+              return NotFound();
+          }
+            var employee = await _context.Employee.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/EmployeeEndPoint/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(int id, Employee employee)
+        {
+            if (id != employee.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/EmployeeEndPoint
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
+        {
+          if (_context.Employee == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Employee'  is null.");
+          }
+            _context.Employee.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmployee", new { id = employee.ID }, employee);
+        }
+
+        // DELETE: api/EmployeeEndPoint/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            if (_context.Employee == null)
+            {
+                return NotFound();
+            }
+            var employee = await _context.Employee.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employee.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            return (_context.Employee?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Data/Migrations/20220528094617_InitialModeledit.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAppConnect.Data.Migrations
+{
+    public partial class InitialModeledit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Employee",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Department",
+                table: "Employee",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DOB",
+                table: "Employee",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DOB",
+                table: "Employee");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Gender",
+                table: "Employee",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Department",
+                table: "Employee",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
